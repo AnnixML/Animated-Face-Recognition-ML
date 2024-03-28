23d78fc61dacc743669bf92410e8f48619871267
diamond0411 <gu314@purdue.edu>
2024-03-28 00:13:46 -0400

    model_endpoint done

---
+++ model_endpoint/model_flask.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify
+import torch
+from torch import nn
+from torchvision import transforms
+import requests
+from transformers import AutoModelForImageClassification
+from PIL import Image
+import csv
+import io
+app = Flask(__name__)
+classes=[]
+file = open("classes.csv", "r")
+reader = csv.reader(file)
+for row in reader:
+    #print(row)
+    classes.append(row[1])
+file.close()
+model = AutoModelForImageClassification.from_pretrained(
+    "microsoft/beit-base-patch16-224-pt22k-ft22k",
+    image_size=224,
+    num_labels=3357,
+    ignore_mismatched_sizes=True,
+)
+weights = torch.load("weights.ckpt")["state_dict"]
+pp = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+])
+w = {}
+for k, v in weights.items():
+    #print(k)
+    if k.startswith("net"):
+        k = k.replace("net"+".", "")
+        w[k] = v
+    '''
+    if "relative_position_bias_table" in k:
+        k = k.replace("bias_table", "index")
+        w[k] = v
+    '''
+model.load_state_dict(w, strict=True)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.data
+    if file:
+        im_data = io.BytesIO(file)
+        image = pp(Image.open(im_data).convert("RGB")).unsqueeze(0)
+        outputs = model(pixel_values=image)
+        logits = outputs.logits
+        probs = torch.nn.functional.softmax(logits, dim=-1)
+        top5_probs, top5_classes = torch.topk(probs, 5)
+        top5_classes=top5_classes[0].tolist()
+        print(top5_classes)
+        print(len(classes))
+        top5_classes = [classes[idx] for idx in top5_classes]
+        return jsonify({
+            'top5_probs': top5_probs[0].tolist(),
+            'top5_classes': top5_classes
+        })
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
